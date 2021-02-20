01adb363735ba4301eb6399e035967f7b311c8db
johannesbyle <johannesbyle1@gmail.com>
2021-02-20 16:32:09 -0600

    refactored from private repo

---
+++ basic_functions.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import tilemapbase
+import numpy as np
+from scipy.ndimage.filters import gaussian_filter
+from matplotlib.cm import get_cmap
+import json
+
+
+def project(longitude, latitude):
+    x_tile = (longitude + 180.0) / 360.0
+    lat_rad = np.radians(latitude)
+    y_tile = (1.0 - np.log(np.tan(lat_rad) + (1 / np.cos(lat_rad))) / np.pi) / 2.0
+    return x_tile, y_tile
+
+
+def opacity(x, c=100, b=1):
+    return np.clip(-1 / (c * x + 1) + b, 0, None)
+
+
+def get_base_map(edges, zoom=12, dpi=600, t=None):
+    tilemapbase.start_logging()
+    tilemapbase.init(create=True)
+    if not t:
+        t = tilemapbase.tiles.Carto_Light_No_Labels
+    extent = tilemapbase.Extent.from_lonlat(edges["west"], edges["east"], edges["south"], edges["north"])
+    fig, ax = plt.subplots(dpi=dpi)
+    ax.xaxis.set_visible(False)
+    ax.yaxis.set_visible(False)
+    plotter = tilemapbase.Plotter(extent, t, zoom=zoom)
+    plotter.plot(ax, t)
+    return ax, extent
+
+
+def load_json(filename, edges):
+    with open(filename) as f:
+        df = pd.DataFrame(json.load(f)["locations"])
+    df["latitudeE7"] = df["latitudeE7"].astype(int) / 10 ** 7
+    df["longitudeE7"] = df["longitudeE7"].astype(int) / 10 ** 7
+    df = df[df["latitudeE7"] <= edges["north"]]
+    df = df[df["latitudeE7"] >= edges["south"]]
+    df = df[df["longitudeE7"] <= edges["east"]]
+    df = df[df["longitudeE7"] >= edges["west"]]
+    return df
+
+
+def plot_heatmap(df, ax, extent, bins=3000, color="viridis", sigma=1, alpha=1):
+    x, y = project(df["longitudeE7"], df["latitudeE7"])
+    scale = (extent.ymax - extent.ymin) / (extent.xmax - extent.xmin)
+    heatmap, x_edges, y_edges = np.histogram2d(x, y, bins=(bins, int(bins * scale)))
+    extent = [x_edges[0], x_edges[-1], y_edges[0], y_edges[-1]]
+    heatmap = gaussian_filter(np.log(heatmap + 1), sigma=sigma)
+    heatmap[heatmap == 0] = np.nan
+    cm = get_cmap(color) if isinstance(color, str) else get_cmap()
+    im = cm(heatmap)
+    if isinstance(color, tuple):
+        im[heatmap != np.nan] = color
+    else:
+        raise Exception("Invalid color: {}".format(color))
+    im[:, :, 3] = opacity(heatmap, b=alpha)
+    ax.imshow(np.transpose(im, (1, 0, 2)), extent=extent, origin='lower')
---
+++ create_map.py
@@ -0,0 +1,56 @@
+from basic_functions import *
+import pandas as pd
+from matplotlib.cm import get_cmap
+import os
+from tqdm import tqdm
+
+os.chdir(os.path.dirname(__file__))
+if not os.path.exists("maps"):
+    os.mkdir("maps")
+
+
+def create_map_from_filenames(filenames, colors, edges, zoom=12, dpi=600, t=None, bins=3000, sigma=1, alpha=1):
+    """
+    :param filenames: List of filenames, either list of strings or list of list of strings
+    :param colors: Colors for each heatmap, either a list of matplotlib colormap strings, a list of tuples, a tuple, or
+    a matplotlib colormap string
+    :param edges: Edges of map, must have form {"north": float, "south": float,"east": float,"west": float}
+    :param zoom: Zoom level of map, default 12
+    :param dpi: Dots-per-inch, default 6000
+    :param t: Type of basemap from tilemapbase.tiles, either from Carto or Stamen, default Carto_Light_No_Labels
+    :param bins: Number of bins in heatmap, i.e. how detailed the heatmap is, default 3000
+    :param sigma: How much smoothing is applied to the heatmap, default is 1
+    :param alpha: Transparency of each heatmap layer, default 1
+    :return:w
+    """
+    if isinstance(colors, list):
+        assert len(colors) == len(filenames)
+    assert all([isinstance(n, type(filenames[0])) for n in filenames])
+    ax, extent = get_base_map(edges, zoom=zoom, dpi=dpi, t=t)
+    dfs = []
+    for filename in tqdm(filenames, desc="Loading data"):
+        if isinstance(filename, str):
+            dfs.append(load_json(filename, edges))
+        elif isinstance(filename, list):
+            dfs.append(pd.concat([load_json(n, edges) for n in filename]))
+        else:
+            raise Exception("Unknown filename: {}".format(filename))
+    for n in tqdm(range(len(dfs)), desc="Generating map"):
+        if isinstance(colors, list):
+            color = colors[n]
+        elif isinstance(colors, tuple):
+            color = colors
+        elif isinstance(colors, str):
+            cm = get_cmap(colors)
+            color = cm(n / (len(dfs) - 1 if len(dfs) > 1 else 1))
+        else:
+            raise Exception("Unknown colors: {}".format(colors))
+        plot_heatmap(dfs[n], ax, extent, bins=bins, sigma=sigma, color=color, alpha=alpha)
+    saved = False
+    n = 0
+    while not saved:
+        save_filename = "maps/map_{}.png".format(n)
+        if not os.path.exists(save_filename):
+            plt.savefig(save_filename)
+            saved = True
+        n += 1
